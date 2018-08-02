9a6cbd26bbf22b6e3e9b6f190df9e69da98b9586
yytx1992 <41986720+yytx1992@users.noreply.github.com>
2018-08-02 17:10:10 -0500

    Modify the structure of the code
    The modification was made with the purpose of fitting the question requirements (e.g. A better way to define different threads).

--- RCG.py
+++ RCG.py
@@ -1,17 +1,22 @@
 #[MLFT] Candidate Question1
 #Author: Yi Yang
-#Date: 2018/08/01
+#Date: 2018/08/02
 #Version: python 3.5
+
 import random,queue,time,threading
-class Random_Count_Generator:
-    def __init__(self):
-        self.history=queue.Queue(maxsize=100)#The queue to store the most last 100 numbers, used in part 2
+
+class Random_Count_Generator(threading.Thread):#The thread to generate numbers
+    def __init__(self,name,delay):
+        threading.Thread.__init__(self)
+        global history
+        global logs
+        self.name=name
         self.probs=[0.5,0.25,0.15,0.05,0.05]
-        self.logs=queue.Queue(maxsize=0)#The queue to store numbers, timestamps and the thread name(for convenience)
         self.lock=threading.Lock()
-    def print_random_num(self,delay):#The thread to generate numbers
+        self.delay=delay
+    def run(self):
         for i in range(2000):#Assume each thread will generate 2000 numbers
-            time.sleep(delay)
+            time.sleep(self.delay)
             with self.lock:
                 r=random.random()
                 timestamp=time.time()
@@ -19,57 +24,64 @@ class Random_Count_Generator:
                 while(r>=0 and index<len(self.probs)):
                   r-=self.probs[index]
                   index+=1
-                if self.history.full():
-                    self.history.get()
-                self.history.put(index)      
-                self.logs.put("Number generated:"+str(index)+"  Time:"+str(timestamp)+"  Thread name:"+str(threading.current_thread().name)+"\n")      
-#            print(index)               
-    def write_to_disk(self):#The thread to write into disk
+                if history.full():
+                    history.get()
+                history.put(index)      
+                logs.put("Number generated:"+str(index)+"  Time:"+str(timestamp)+"  Thread name:"+str(threading.current_thread().name)+"\n")      
+#            print(index)   
+                
+class Write(threading.Thread):#The thread to write into disk
+    def __init__(self,name):
+        threading.Thread.__init__(self)
+        global history
+        global logs 
         self.file=open("data.txt","a+")
-        count=0
+        self.count=0
+        self.name=name          
+    def run(self):
         try:                
             while True:
-                text=self.logs.get(True,1)#time_out==1s
+                text=logs.get(True,1)#time_out==1s
                 self.file.write(text)
-                count+=1
+                self.count+=1
         except:#All data has been written into the file
-#            print ("Total count:",count)
+#            print ("Total count:",self.count)
             self.file.close()
             print ("Frequency percentages of each number for the last 100 numbers:",self.return_frequency())
     def return_frequency(self):#Return the frequency percentages of each number for the last 100 numbers
         fq={}
-        count=[0]*5
-        while not self.history.empty():
-            get=self.history.get()
+        self.count=[0]*5
+        while not history.empty():
+            get=history.get()
             if get==1:
-                count[0]+=1
+                self.count[0]+=1
             elif get==2:
-                count[1]+=1
+                self.count[1]+=1
             elif get==3:
-                count[2]+=1
+                self.count[2]+=1
             elif get==4:
-                count[3]+=1
+                self.count[3]+=1
             elif get==5:
-                count[4]+=1           
-        total=sum(count)      
-        for idx,val in enumerate(count,start=1):
+                self.count[4]+=1           
+        total=sum(self.count)      
+        for idx,val in enumerate(self.count,start=1):
             fq[idx]=float(val)/total
-        return fq          
-    def run(self):#Build threads and run them
-        t1=threading.Thread(target=self.print_random_num,name="t1",args=(0,))#Assume the delay time for each generation is 0s
-        t2=threading.Thread(target=self.print_random_num,name="t2",args=(0,))
-        t3=threading.Thread(target=self.print_random_num,name="t3",args=(0,))
-        t4=threading.Thread(target=self.print_random_num,name="t4",args=(0,))
-        t5=threading.Thread(target=self.print_random_num,name="t5",args=(0,))    
-        w=threading.Thread(target=self.write_to_disk,name="w")
-        t1.start()
-        t2.start()
-        t3.start()
-        t4.start()
-        t5.start()
-        w.start()        
+        return fq                 
         
 if __name__ == "__main__":  
     open('data.txt', 'w').close()
-    RCG=Random_Count_Generator()
-    RCG.run()
+    history=queue.Queue(maxsize=100)#The queue to store the most last 100 numbers, used in part 2
+    logs=queue.Queue(maxsize=0)#The queue to store numbers, timestamps and the thread name(for convenience)
+    thread1=Random_Count_Generator("Thread-1",0)#Assume the delay time for each generation is 0s
+    thread2=Random_Count_Generator("Thread-2",0)
+    thread3=Random_Count_Generator("Thread-3",0)
+    thread4=Random_Count_Generator("Thread-4",0)
+    thread5=Random_Count_Generator("Thread-5",0)
+    threadW=Write("Thread-W")
+    #Run the threads
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
+    threadW.start()
